c5cc3607c3240c905ea76999c8a0b131a16e2f4e
Yariazen <35210313+Yariazen@users.noreply.github.com>
2024-08-27 10:25:55 -0700

    Test Generate References

---
+++ GenerateReferences/Class1.cs
@@ -0,0 +1,23 @@
+﻿using Kitchen;
+using KitchenData;
+using KitchenMods;
+
+namespace GenerateReferences
+{
+    public class GenerateReferencesSystem : GenericSystemBase, IModSystem
+    {
+        protected override void OnUpdate()
+        {
+            
+        }
+
+        protected override void Initialise()
+        {
+            Debug.Log("GenerateReferencesSystem Initialised");
+            foreach (GameDataObject gdo in GameData.Main.Get<GameDataObject>())
+            {
+                Debug.Log(gdo.name);
+            }
+        }
+    }
+}
